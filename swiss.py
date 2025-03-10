4e3c984cf58a41ac1c61b65ee6533286d0dfeca6
Greg Finley <gregory.finley@gmail.com>
2025-03-10 00:28:06 -0700

    get more and only made by me

--- swiss.py
+++ swiss.py
@@ -134,7 +134,7 @@ def create_tournament(start_time: str, api_key: str, tournament_config: Tourname
 def process_tourney_config(tournament_config: TournamentConfig, api_key: str) -> None:
     # https://lichess.org/api#tag/Swiss-tournaments/operation/apiTeamSwiss
     swisses = requests.get(
-        f"https://lichess.org/api/team/{tournament_config.path_param}/swiss?max=200",
+        f"https://lichess.org/api/team/{tournament_config.path_param}/swiss?max=500",
         headers={"Authorization": f"Bearer {api_key}"},
         stream=True
     )
@@ -144,9 +144,7 @@ def process_tourney_config(tournament_config: TournamentConfig, api_key: str) ->
     for line in swisses.iter_lines():
         if line:
             swiss = json.loads(line)
-            if (swiss['status'] == 'created' and swiss['name'] == tournament_config.name and 
-                    # Ignore a bunch of junk far in the future
-                    ((not tournament_config.name.startswith('Hourly')) or swiss['startsAt'] <= '2025-03-10T03:15:00Z') or swiss['createdBy'] == 'gbfgbfgbf'):
+            if (swiss['status'] == 'created' and swiss['name'] == tournament_config.name and swiss['createdBy'] == 'gbfgbfgbf'):
                 created_count += 1
                 if first_start_time is None:
                     first_start_time = swiss['startsAt']
